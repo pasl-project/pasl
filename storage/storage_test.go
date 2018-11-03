1e196819bd4a12f1ee9c4308374ff1bba340a139
xiphon <xiphon@protonmail.com>
2018-11-03 15:40:49 +0000

    storage.test: add FailOpen test, fix WriteRead test, increase overall coverage

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -13,38 +13,72 @@ func WriteREadTest(t *testing.T, flush bool) {
 	var txRipemd160Hash [20]byte
 	txHash, _ := hex.DecodeString("1234567890123456789012345678901234567890")
 	copy(txRipemd160Hash[:], txHash[:])
+	var txRipemd160Hash2 [20]byte
+	txHash2, _ := hex.DecodeString("7890123456789012345678901234567890123456")
+	copy(txRipemd160Hash2[:], txHash2[:])
+	var txRipemd160Hash3 [20]byte
+	txHash3, _ := hex.DecodeString("8901234567890123456789012345678901234567")
+	copy(txRipemd160Hash3[:], txHash3[:])
 	serializedAccount, _ := hex.DecodeString("3456789012345678901234567890123456789012")
+	serializedAccount2, _ := hex.DecodeString("6789012345678901234567890123456789012345")
 	serializedBlock, _ := hex.DecodeString("4567890123456789012345678901234567890123")
 	serializedTx, _ := hex.DecodeString("2345678901234567890123456789012345678901")
+	serializedTx2, _ := hex.DecodeString("5678901234567890123456789012345678901234")
 	number := uint32(0x19001233)
+	number2 := uint32(0x19001244)
 	operationIndex := uint32(0x01293818)
+	operationIndex2 := uint32(0x01293814)
 
-	WithStorage(&dbFileName, 5, func(storage *Storage) error {
+	defer os.Remove(dbFileName)
+	if WithStorage(&dbFileName, 5, func(storage *Storage) error {
 		storage.Store(0, serializedBlock,
 			func(txes func(txRipemd160Hash [20]byte, txData []byte)) {
 				txes(txRipemd160Hash, serializedTx)
+				txes(txRipemd160Hash2, serializedTx2)
 			},
 			func(accountOperations func(number uint32, internalOperationId uint32, txRipemd160Hash [20]byte)) {
 				accountOperations(number, operationIndex, txRipemd160Hash)
+				accountOperations(number2, operationIndex2, txRipemd160Hash2)
 			},
 			func(fn func(number uint32, data []byte) error) error {
-				return fn(number, serializedAccount)
+				if err := fn(number, serializedAccount); err != nil {
+					t.FailNow()
+				}
+				if err := fn(number2, serializedAccount2); err != nil {
+					t.FailNow()
+				}
+				return nil
 			})
+
 		if flush {
 			storage.flush()
 		}
-		txes, err := storage.GetAccountTxes(number)
-		if err != nil {
-			t.FailNow()
+
+		{
+			txesData, err := storage.GetAccountTxesData(number)
+			if err != nil {
+				t.FailNow()
+			}
+			if txData, ok := txesData[operationIndex]; !ok {
+				t.FailNow()
+			} else if bytes.Compare(txData[:], serializedTx[:]) != 0 {
+				t.FailNow()
+			}
 		}
-		if txHash, ok := txes[operationIndex]; !ok {
-			t.FailNow()
-		} else if bytes.Compare(txHash[:], txRipemd160Hash[:]) != 0 {
-			t.FailNow()
+
+		{
+			if txData, err := storage.GetTx(txRipemd160Hash); txData == nil || err != nil {
+				t.FailNow()
+			}
+			if txData, err := storage.GetTx(txRipemd160Hash3); txData != nil || err == nil {
+				t.FailNow()
+			}
 		}
+
 		return nil
-	})
-	os.Remove(dbFileName)
+	}) != nil {
+		t.FailNow()
+	}
 }
 
 func TestWriteFlushRead(t *testing.T) {
@@ -55,4 +89,11 @@ func TestWriteRead(t *testing.T) {
 	WriteREadTest(t, false)
 }
 
+func TestFailOpen(t *testing.T) {
+	dbFileName := ":"
+	if err := WithStorage(&dbFileName, 5, func(storage *Storage) error {
+		return nil
+	}); err == nil {
+		t.FailNow()
+	}
 }
